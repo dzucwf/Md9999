23028e4ef53c8275d99cf19d7d240c7df83b7e39
dzucwf <dz_uc@163.com>
2015-09-29 10:06:46 +0800

    测试框架

--- app/src/main/java/com/md9999/MainActivity.java
+++ app/src/main/java/com/md9999/MainActivity.java
@@ -1,17 +1,32 @@
 package com.md9999;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.lidroid.xutils.view.annotation.event.OnClick;
 
 public class MainActivity extends ActionBarActivity {
 
-    //
+    @ViewInject(R.id.button)
+   private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewUtils.inject(this);
+    }
+
+    @OnClick(R.id.button)
+    public void onClick(View view){
+        Toast.makeText(this,"test",Toast.LENGTH_SHORT).show();
     }
 
     @Override
