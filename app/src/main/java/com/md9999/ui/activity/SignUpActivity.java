a5404873a36cbc4696b4999d90aa1aa5cc2adcf3
Lionnd <lionnd@gmail.com>
2015-09-29 22:09:19 +0800

    添加註冊頁面 修改主頁的下边栏 添加我的医生界面（半成品）

--- app/src/main/java/com/md9999/ui/activity/SignUpActivity.java
+++ app/src/main/java/com/md9999/ui/activity/SignUpActivity.java
@@ -12,29 +12,22 @@ import com.md9999.R;
 /**
  * Created by Lionnd on 2015/9/29.
  */
-public class LoginAcitvity extends FragmentActivity {
+public class SignUpActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
         ViewUtils.inject(this);
         initialView();
     }
     private void initialView() {
 
     }
-    @OnClick(R.id.loginActivity_btn_login)
+    @OnClick(R.id.signUpActivity_btn_back)
     public void btn_loginClicked(View v){
         Intent intent = new Intent();
-        intent.setClass(LoginAcitvity.this, MainActivity.class);
+        intent.setClass(SignUpActivity.this,LoginAcitvity.class);
         startActivity(intent);
     }
-    @OnClick(R.id.loginActivity_btn_signUp)
-    public void btn_signUpClicked(View v){
-        Intent intent = new Intent();
-        intent.setClass(LoginAcitvity.this, SignUpActivity.class);
-        startActivity(intent);
-    }
-
 }
