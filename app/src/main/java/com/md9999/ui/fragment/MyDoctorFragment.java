d0332764b7b5856a07a9308ddc4fed66b0019a5f
Lionnd <lionnd@gmail.com>
2015-09-30 09:51:01 +0800

    完成服务主页布局

--- app/src/main/java/com/md9999/ui/fragment/MyDoctorFragment.java
+++ app/src/main/java/com/md9999/ui/fragment/MyDoctorFragment.java
@@ -24,7 +24,7 @@ public class MyDoctorFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_service, container, false);
+        return inflater.inflate(R.layout.fragment_my_doctor, container, false);
     }
 
 
